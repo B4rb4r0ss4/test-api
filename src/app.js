209d716fd149bb4262b39be14cd011d30b0fc00f
lukasz <lukasz.stodolka@op.pl>
2021-07-27 20:24:46 +0200

    Added public folder

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const testRouter = require('./routers/tests');
+const path = require('path');
 require('./db/mongoose');
 
+const publicDirectoryPath = path.join(__dirname, '../public');
 const app = express();
+app.use(express.static(publicDirectoryPath));
 
 app.use(express.json());
 app.use(testRouter);
-
 module.exports = app;
